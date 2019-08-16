38552a5783daa188cc3b6caf40822c596ccac605
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 11:20:48 -0400

    initial commit

---
+++ src/main/java/com/lambdaschool/todoer/TodoerApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.todoer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TodoerApplication.class, args);
+    }
+
+}
