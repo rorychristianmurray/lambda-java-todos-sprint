b43f305108292acaa40fd28f0efc957995dcd14a
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 11:32:16 -0400

    Build User model

--- src/main/java/com/lambdaschool/todoer/model/User.java
+++ src/main/java/com/lambdaschool/todoer/model/User.java
@@ -1,5 +1,132 @@
 package com.lambdaschool.todoer.model;
 
-public class User
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name ="users")
+
+public class User extends Auditable
 {
+    // primary key - joins to todos
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(nullable = false,
+            unique = true)
+    private String username; // not null unique
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password; // not null
+
+    // one user to many roles mapping
+    @OneToMany(mappedBy = "user",
+            cascade =CascadeType.ALL)
+    @JsonIgnoreProperties("user")
+    private List<UserRoles> userRoles = new ArrayList<>();
+
+
+    // one user to many todos mapping
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    @JsonIgnoreProperties("user")
+    private List<Todo> todos = new ArrayList<>();
+
+    // generate base constructor
+    public User()
+    {
+    }
+
+    // generate regular constructor
+
+
+    public User(String username, String password, List<UserRoles> userRoles)
+    {
+        setUsername(username); // *** REVIEW THESE ***
+        setPassword(password);
+        for (UserRoles ur : userRoles)
+        {
+            ur.setUser(this);
+        }
+        this.userRoles = userRoles;
+    }
+
+    // generate getters and setters
+
+
+    public long getUserid()
+    {
+        return userid;
+    }
+
+    public void setUserid(long userid)
+    {
+        this.userid = userid;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void setPasswordNoEncrypt(String password)
+    {
+        this.password = password;
+    }
+
+    public List<UserRoles> getUserRoles()
+    {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles)
+    {
+        this.userRoles = userRoles;
+    }
+
+    public List<Todo> getTodos()
+    {
+        return todos;
+    }
+    public void setTodos(List<Todo> todos)
+    {
+        this.todos = todos;
+    }
+
+    public List<SimpleGrantedAuthority> getAuthority()
+    {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+        for (UserRoles r : this.userRoles)
+        {
+            String myRole = "ROLE_" + r.getRole().getRolename().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+        return rtnList;
+    }
 }
