69acf0d2e07a1537d8a6f8eaf5be7d1d37231044
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 11:31:23 -0400

    build Role model

--- src/main/java/com/lambdaschool/todoer/model/Role.java
+++ src/main/java/com/lambdaschool/todoer/model/Role.java
@@ -1,5 +1,73 @@
 package com.lambdaschool.todoer.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
 public class Role
 {
+    // primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long roleid;
+
+    @Column(nullable = false,
+            unique = true)
+    private String rolename; // not null - unique
+
+    // one role maps to many user roles
+    @OneToMany(mappedBy = "role",
+            cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("role")
+    private List<UserRoles> userRoles = new ArrayList<>();
+
+    // generate base constructor
+    public Role()
+    {
+    }
+
+    // generate regular constructor
+
+
+    public Role(String rolename)
+    {
+        this.rolename = rolename;
+    }
+
+    // generate getters and setters
+
+    public long getRoleid()
+    {
+        return roleid;
+    }
+
+    public void setRoleid(long roleid)
+    {
+        this.roleid = roleid;
+    }
+
+    public String getRolename()
+    {
+        return rolename;
+    }
+
+    public void setRolename(String rolename)
+    {
+        this.rolename = rolename;
+    }
+
+    public List<UserRoles> getUserRoles()
+    {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles)
+    {
+        this.userRoles = userRoles;
+    }
 }
+
