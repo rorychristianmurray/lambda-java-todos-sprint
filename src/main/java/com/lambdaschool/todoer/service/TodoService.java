bdea8685b0741ca11ed32499e4b7886d80a46580
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 12:01:06 -0400

    Build TodoService service

--- src/main/java/com/lambdaschool/todoer/service/TodoService.java
+++ src/main/java/com/lambdaschool/todoer/service/TodoService.java
@@ -1,5 +1,24 @@
 package com.lambdaschool.todoer.service;
 
+import com.lambdaschool.todoer.model.Todo;
+import com.lambdaschool.todoer.view.CountTodos;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public interface TodoService
 {
+    List<Todo> findAll();
+
+    Todo findTodoById(long id);
+
+    List<Todo> findByUserName (String username);
+
+    void delete(long id);
+
+    Todo save(Todo todo);
+
+    Todo update(Todo quote, long id);
+
+    ArrayList<CountTodos> getCountQuotes();
 }
