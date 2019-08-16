42a671c24dd91b99ff2848a39c6c71617f88ef4a
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 11:55:19 -0400

    Build RoleService

--- src/main/java/com/lambdaschool/todoer/service/RoleService.java
+++ src/main/java/com/lambdaschool/todoer/service/RoleService.java
@@ -1,5 +1,16 @@
 package com.lambdaschool.todoer.service;
 
+import com.lambdaschool.todoer.model.Role;
+
+import java.util.List;
+
 public interface RoleService
 {
+    List<Role> findAll();
+
+    Role findRoleById(long id);
+
+    void delete(long id);
+
+    Role save(Role role);
 }
