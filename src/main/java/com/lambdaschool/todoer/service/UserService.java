fb7141b2980b10a8825325db83e5344f9e82cfe8
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 11:57:29 -0400

    Build UserService service

--- src/main/java/com/lambdaschool/todoer/service/UserService.java
+++ src/main/java/com/lambdaschool/todoer/service/UserService.java
@@ -1,5 +1,20 @@
 package com.lambdaschool.todoer.service;
 
-public interface UserService
-{
+import com.lambdaschool.todoer.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User findUserByName(String name);
+
+    User findUserById(long id);
+
+    void delete(long id);
+
+    User save(User user);
+
+    User update(User user, long id);
 }
