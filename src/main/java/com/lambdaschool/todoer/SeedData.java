c30ff8072e7a2c7260caf0abb25cc3bf5a67015a
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 12:28:55 -0400

    Add SeedData

---
+++ src/main/java/com/lambdaschool/todoer/SeedData.java
@@ -0,0 +1,67 @@
+package com.lambdaschool.todoer;
+
+// provided by Vivek Vishwanath
+
+import com.lambdaschool.todoer.model.Role;
+import com.lambdaschool.todoer.model.Todo;
+import com.lambdaschool.todoer.model.User;
+import com.lambdaschool.todoer.model.UserRoles;
+import com.lambdaschool.todoer.repository.RoleRepository;
+import com.lambdaschool.todoer.repository.TodoRepository;
+import com.lambdaschool.todoer.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Transactional
+@Component
+public class SeedData implements CommandLineRunner
+{
+    RoleRepository rolerepos;
+    UserRepository userrepos;
+    TodoRepository todorepos;
+
+    public SeedData(RoleRepository rolerepos, UserRepository userrepos, TodoRepository todorepos) {
+        this.rolerepos = rolerepos;
+        this.userrepos = userrepos;
+        this.todorepos = todorepos;
+    }
+
+    @Override
+    public void run(String[] args) throws Exception {
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+
+        rolerepos.save(r1);
+        rolerepos.save(r2);
+
+        ArrayList<UserRoles> users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u1 = new User("barnbarn", "ILuvM4th!", users);
+        u1.getTodos().add(new Todo("Finish java-orders-swagger", new Date(), u1));
+        u1.getTodos().add(new Todo("Feed the turtles", new Date(), u1));
+        u1.getTodos().add(new Todo("Complete the sprint challenge", new Date(), u1));
+        userrepos.save(u1);
+
+        ArrayList<UserRoles> admins = new ArrayList<>();
+        admins.add(new UserRoles(new User(), r1));
+        admins.add(new UserRoles(new User(), r2));
+        User u2 = new User("admin", "password", admins);
+        u2.getTodos().add(new Todo("Walk the dogs", new Date(), u2));
+        u2.getTodos().add(new Todo("provide feedback to my instructor", new Date(), u2));
+        userrepos.save(u2);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u3 = new User("Bob", "password", users);
+        userrepos.save(u3);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u4 = new User("Jane", "password", users);
+        userrepos.save(u4);
+    }
+}
