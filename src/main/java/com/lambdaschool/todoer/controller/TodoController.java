a525e20951617ce9b8a35fc66487407c45b9e9d6
Rory Murray <rory.christian.murray@gmail.com>
2019-08-16 13:02:22 -0400

    Build TodoController

--- src/main/java/com/lambdaschool/todoer/controller/TodoController.java
+++ src/main/java/com/lambdaschool/todoer/controller/TodoController.java
@@ -1,5 +1,27 @@
 package com.lambdaschool.todoer.controller;
 
+import com.lambdaschool.todoer.model.Todo;
+import com.lambdaschool.todoer.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController
 {
+    @Autowired
+    TodoService todoService;
+
+    @PutMapping(value = "/todo/{todoid}")
+    public ResponseEntity<?> updateTodo(
+            @RequestBody
+                    Todo updateTodo,
+            @PathVariable
+                    long todoid)
+    {
+        todoService.update(updateTodo, todoid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
